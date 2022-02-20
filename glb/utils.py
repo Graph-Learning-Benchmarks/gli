63f78498b8fdec388b0cf5989b49e8b2d3f7ad42
Xingjian Zhang <jimmyzxj@umich.edu>
2022-02-20 17:07:36 -0500

    Dataloader (#2)
    * Add Graph class
    
    * Add print info
    
    * Pass pylint check
    
    * Create pylint.yml
    
    * Update pylint.yml
    
    * remove attribute.py
    
    * Update pylint.yml
    
    * Change name DenseArray to Tensor (#3)
    
    * Change name DenseArray to Tensor
    
    * Add task.json and add json dump indents
    
    * Format with yapf
    
    * Pass pycodestyle and pydocstyle
    
    * Add example pipeline for cora
    
    * Pass py code check
    
    * Pass style
    
    * Fix codestyle
    
    * Fix code style and use google pylintrc
    
    * Pass code style.
    
    Former-commit-id: 789ee1fe013715cb453727a5ff26c5fea8ecd8f5

---
+++ glb/utils.py
@@ -0,0 +1,32 @@
+"""Utility functions."""
+import os
+import numpy as np
+import scipy.sparse as sp
+
+
+def load_data(path: os.PathLike):
+    """Load data from given path.
+
+    Supported file format:
+    1. .npz
+    2. .npy
+    """
+    _, ext = os.path.splitext(path)
+    if ext in (".npz", ".npy"):
+        data = np.load(path, allow_pickle=True)
+    else:
+        raise NotImplementedError(f"{ext} file is currently not supported.")
+    return data
+
+
+def is_raw_sparse(array):
+    """Return true if array is sparse.
+
+    This method is to deal with the situation where array is loaded from
+    sparse matrix by np.load(), which will wrap array to be a numpy.ndarray.
+    """
+    if sp.issparse(array):
+        return True
+    if isinstance(array, np.ndarray):
+        return array.dtype.kind not in set("buifc")
+    raise TypeError
