0c6f1e702c311cfb668d3186447af5c6dca70471
Xingjian Zhang <jimmyzxj@umich.edu>
2022-03-10 23:19:08 -0500

    Add docstring for example.py

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
+"""Example of data loading for users."""
 import glb
 
 g = glb.graph.read_glb_graph(metadata_path="./examples/cora/metadata.json")
-task = glb.dataset.read_glb_task(task_path="./examples/cora/task.json")
+task = glb.task.read_glb_task(task_path="./examples/cora/task.json")
 
 dataset = glb.dataloading.combine_graph_and_task(g, task)
 
