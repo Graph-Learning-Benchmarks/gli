d779f8ea086813faad8df7a267f96f55aa5ac1e1
Xingjian Zhang <jimmyzxj@umich.edu>
2022-03-27 23:08:43 -0400

    Feature/read task (#15)
    * :
    
    * Update task read format for cora as an example
    
    * Add additional comments
    
    * Add methods for GraphClassification
    
    * Fix GraphClassification example
    
    * Fix graph classification example
    
    * Fix code style check
    
    * Fix example.py code style
    
    Former-commit-id: 52b132d65d228095cfdc73a27f5c59866fd0dcfe

--- glb/graph.py
+++ glb/graph.py
@@ -15,6 +15,7 @@ from .utils import load_data, is_sparse
 def is_single_graph(data):
     """Return true if the glb data contains a single graph."""
     nodelist = data["Graph"]["_NodeList"]
+    nodelist = torch.squeeze(nodelist)
     if nodelist.dim() == 1:
         return True
     elif nodelist.dim() == 2:
@@ -23,27 +24,54 @@ def is_single_graph(data):
         raise ValueError("_NodeList has more than 2 dimensions.")
 
 
+def is_hetero_graph(data):
+    """Return true if the glb data contains heterogeneous graph."""
+    depth = _dict_depth(data)
+    # Heterogeneous graph has one more depth than a homogeneous one.
+    if depth == 5:
+        return True
+    elif depth == 4:
+        return False
+    else:
+        raise RuntimeError("metadata.json has wrong structure.")
+
+
 def get_single_graph(data, device="cpu"):
     """Initialize and return a single Graph instance given data."""
     edges = data["Edge"].pop("_Edge")  # (num_edges, 2)
-    node_list = data["Graph"]["_NodeList"]
     src_nodes, dst_nodes = edges.T[0], edges.T[1]
-    num_nodes = node_list.shape[-1]
 
-    g: dgl.DGLGraph = dgl.graph((src_nodes, dst_nodes), num_nodes=num_nodes,
-                                device=device)
+    g: dgl.DGLGraph = dgl.graph((src_nodes, dst_nodes), device=device)
     for attr, array in data["Node"].items():
         g.ndata[attr] = array
 
     for attr, array in data["Edge"].items():
-        g.edges[attr] = array
+        g.edata[attr] = array
 
     return g
 
 
 def get_multi_graph(data, device="cpu"):
     """Initialize and return a list of Graph instance given data."""
-    raise NotImplementedError
+    node_list = data["Graph"]["_NodeList"]
+    edge_list = data["Graph"].get("_EdgeList", None)
+    graphs = []
+
+    # Extract the whole graph
+    g: dgl.DGLGraph = get_single_graph(data, device)
+    if edge_list:
+        node_list = node_list.bool()
+        assert edge_list.dim() == 2, "_EdgeList should be a matrix."
+        edge_list = edge_list > 0
+        for i in range(len(edge_list)):
+            graphs.append(dgl.edge_subgraph(g, edge_list[i]))
+    else:
+        node_list = node_list.bool()
+        assert node_list.dim() == 2, "_NodeList should be a matrix."
+        for i in range(len(node_list)):
+            graphs.append(dgl.node_subgraph(g, node_list[i]))
+
+    return graphs
 
 
 def read_glb_graph(metadata_path: os.PathLike, device="cpu", verbose=True):
@@ -64,6 +92,9 @@ def read_glb_graph(metadata_path: os.PathLike, device="cpu", verbose=True):
     assert "_Edge" in metadata["data"]["Edge"]
     assert "_NodeList" in metadata["data"]["Graph"]
 
+    if is_hetero_graph(metadata):
+        raise NotImplementedError("Does not support Heterogeneous graph yet.")
+
     data_buffer = {}
     data = {"Node": {}, "Edge": {}, "Graph": {}}
     for neg in ["Node", "Edge", "Graph"]:
@@ -81,6 +112,8 @@ def read_glb_graph(metadata_path: os.PathLike, device="cpu", verbose=True):
                 else:
                     array = raw
                 if is_sparse(array):
+                    # REVIEW - efficiency - this step convert array to dense
+                    # TODO - consider sparse case
                     array = array.all().toarray()
                 array = torch.from_numpy(array).to(device=device)
                 data[neg][attr] = array
@@ -89,3 +122,10 @@ def read_glb_graph(metadata_path: os.PathLike, device="cpu", verbose=True):
         return get_single_graph(data, device)
     else:
         return get_multi_graph(data, device)
+
+
+def _dict_depth(d):
+    """Return the depth of a dictionary."""
+    if isinstance(d, dict):
+        return 1 + (max(map(_dict_depth, d.values())) if d else 0)
+    return 0
