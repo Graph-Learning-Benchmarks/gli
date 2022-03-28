52b132d65d228095cfdc73a27f5c59866fd0dcfe
Xingjian Zhang <jimmyzxj@umich.edu>
2022-03-27 23:08:43 -0400

    Feature/read task (#15)
    * :
    
    * Update task read format for cora as an example
    
    * Add additional comments
    
    * Add methods for GraphClassification
    
    * Fix GraphClassification example
    
    * Fix graph classification example
    
    * Fix code style check
    
    * Fix example.py code style

--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 """Example of data loading for users."""
 import glb
 
-g = glb.graph.read_glb_graph(metadata_path="./examples/cora/metadata.json")
-task = glb.task.read_glb_task(task_path="./examples/cora/task.json")
+metadata_path = "examples/ogb_data/graph_prediction/ogbg-molhiv/metadata.json"
+task_path = "examples/ogb_data/graph_prediction/ogbg-molhiv/task.json"
+g = glb.graph.read_glb_graph(metadata_path=metadata_path)
+task = glb.task.read_glb_task(task_path=task_path)
 
-dataset = glb.dataloading.combine_graph_and_task(g, task)
+datasets = glb.dataloading.combine_graph_and_task(g, task)
 
-g = dataset[0]
-print(g)
+print(datasets)
