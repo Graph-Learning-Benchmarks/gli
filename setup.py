2bd4d539dffb8416a6ad1bd638b107128d9ac7c0
Xingjian Zhang <jimmyzxj@umich.edu>
2022-06-23 23:21:31 -0400

    Add requirement spec (#66)
    Former-commit-id: fa0215213c593fdc9a19655d4d2c2138c5b9a715

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fp:
     desc = fp.read()
 
-setup(
-    name="glb",
-    version="0.1",
-    packages=find_packages(where="glb"),
-    description=desc
-)
+install_requires = []
+
+setup(name="glb",
+      version="0.1",
+      packages=find_packages(where="glb"),
+      install_requires=[
+        "numpy",
+        "scipy",
+        "torch",
+        "dgl-cu102"
+      ],
+      description=desc)
