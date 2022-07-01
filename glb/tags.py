74f45c8f7e7110dd06cc8be7d0e880db033e4f7d
tingwl0122 <107650157+tingwl0122@users.noreply.github.com>
2022-07-01 03:46:00 -0400

    Add files via upload

--- glb/tags.py
+++ glb/tags.py
@@ -176,7 +176,8 @@ def prepare_dataset(dataset, task):
     """Prepare datasets."""
     glb_graph = glb.dataloading.get_glb_graph(dataset)
     glb_task = glb.dataloading.get_glb_task(dataset, task)
-    glb_graph_task = glb.dataloading.combine_graph_and_task(glb_graph, glb_task)
+    glb_graph_task = glb.dataloading.combine_graph_and_task(
+        glb_graph, glb_task)
     return glb_graph, glb_task, glb_graph_task
 
 
