dfa12d1e89f1f501049f6f85d9eecd829d4849eb
Hezheng Fan <hzfan@umich.edu>
2022-07-19 01:15:09 -0400

    add public functions in utils.py

---
+++ tests/utils.py
@@ -0,0 +1,35 @@
+"""Common functions for testing"""
+import os
+
+CURRENT_SUPPORT_TASKS = [
+    "NodeClassification",
+    "GraphClassification",
+    "TimeDependentLinkPrediction",
+    "EntityLinkPrediction",
+    "RelationLinkPrediction",
+]
+
+def example_dir_check(path_to_parent):
+    """Check if a directory has metadata.json.
+
+    If has then it is a example dir.
+    """
+    for fname in os.listdir(path_to_parent):
+        if fname == "metadata.json":
+            return True
+    return False
+
+def find_examples_dir():
+    """Recursively find example directories which have metadata json."""
+    walk_dir = os.getcwd() + "/examples"
+
+    print("walk_dir = " + walk_dir)
+
+    print("walk_dir (absolute) = " + os.path.abspath(walk_dir))
+
+    example_dir_list = []
+    for root, subdirs, _ in os.walk(walk_dir):
+        for subdir in subdirs:
+            if example_dir_check(root + "/" + subdir):
+                example_dir_list.append(root + "/" + subdir)
+    return example_dir_list
