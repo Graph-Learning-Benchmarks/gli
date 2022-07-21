e043587cdd54e6467683c0ce2f77381f25b7a099
StevenGolden1203 <84695981+StevenGolden1203@users.noreply.github.com>
2022-07-21 22:28:48 +0800

    Benchmarking (#119)
    * add train and gcn
    
    * add train, gcn and gat
    
    * choose split 0 for multi-split, add reference
    
    * avoid -1 labels; to_dense on graph
    
    * fix style
    
    * add monet, graphSAGE, add utils for benchmarking
    
    * fix style
    
    * add MLP, fix style
    
    Co-authored-by: Jin Huang <huangjin@gl-login2.arc-ts.umich.edu>
    Co-authored-by: Jin Huang <huangjin@gl-login1.arc-ts.umich.edu>

---
+++ benchmarks/NodeClassification/utils.py
@@ -0,0 +1,59 @@
+"""Utility functions."""
+import torch.nn.functional as F
+from models.gcn import GCN
+from models.gat import GAT
+from models.monet import MoNet
+from models.graph_sage import GraphSAGE
+from models.mlp import MLP
+
+
+def generate_model(args, g, in_feats, n_classes):
+    """Generate required model."""
+    # create model
+    if args.model == "GCN":
+        model = GCN(g,
+                    in_feats,
+                    args.num_hidden,
+                    n_classes,
+                    args.num_layers,
+                    F.relu,
+                    args.in_drop)
+    elif args.model == "GAT":
+        heads = ([args.num_heads] * (args.num_layers-1)) + [args.num_out_heads]
+        model = GAT(g,
+                    args.num_layers,
+                    in_feats,
+                    args.num_hidden,
+                    n_classes,
+                    heads,
+                    F.elu,
+                    args.in_drop,
+                    args.attn_drop,
+                    args.negative_slope,
+                    args.residual)
+    elif args.model == "MoNet":
+        model = MoNet(g,
+                      in_feats,
+                      args.num_hidden,
+                      n_classes,
+                      args.num_layers,
+                      args.pseudo_dim,
+                      args.num_kernels,
+                      args.in_drop)
+    elif args.model == "GraphSAGE":
+        model = GraphSAGE(g,
+                          in_feats,
+                          args.num_hidden,
+                          n_classes,
+                          args.num_layers,
+                          F.relu,
+                          args.in_drop,
+                          args.aggregator_type)
+    elif args.model == "MLP":
+        model = MLP(in_feats,
+                    args.num_hidden,
+                    n_classes,
+                    args.num_layers,
+                    F.relu,
+                    args.in_drop)
+    return model
