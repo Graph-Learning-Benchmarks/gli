96fb26f59bafdcf0b9c1309527371b64257d158b
Xingjian Zhang <jimmyzxj@umich.edu>
2022-08-21 15:31:31 -0400

    Add test group in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,25 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fp:
     desc = fp.read()
 
-install_requires = []
+install_requires = [
+    "numpy>=1.19",
+    "scipy>=1.5",
+    "torch>=1.10",
+    "dgl>=0.6",
+]
+
+test_requires = ["pytest", "pydocstyle", "pycodestyle", "pylint"]
+
+full_requires = install_requires
 
 setup(name="glb",
       version="0.1",
       packages=find_packages(where="glb"),
-      install_requires=[
-          "numpy>=1.19",
-          "scipy>=1.5",
-          "torch>=1.10",
-          "dgl>=0.6",
-      ],
+      install_requires=install_requires,
+      extras_require={
+          "test": test_requires,
+          "full": full_requires
+      },
       python_requires=">=3.6",
       description=desc)
 
