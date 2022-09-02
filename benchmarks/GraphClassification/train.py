f03a7075da9b364da6558ce9b33c06d8726306be
StevenGolden1203 <stevengolden@163.com>
2022-09-02 04:13:35 +0000

    add gin, train for graph classification, utils

--- benchmarks/GraphClassification/train.py
+++ benchmarks/GraphClassification/train.py
@@ -1,1 +1,107 @@
-"""Training code for GraphClassification benchmarks."""
+"""
+Training function in gli for graph classification.
+
+References:
+https://github.com/dmlc/dgl/tree/master/examples/pytorch/gin
+"""
+
+import torch
+from torch import nn
+from torch import optim
+import gli
+from utils import generate_model, load_config_file,\
+                  set_seed, parse_args
+from dgl.dataloading import GraphDataLoader
+
+
+def evaluate(dataloader, device, model):
+    """Evaluate model."""
+    model.eval()
+    total = 0
+    total_correct = 0
+    for batch, (batched_graph, labels) in enumerate(dataloader):
+        batched_graph = batched_graph.to(device)
+        labels = labels.to(device)
+        feat = batched_graph.ndata["NodeFeature"].float()
+        total += len(labels)
+        logits = model(batched_graph, feat)
+        _, predicted = torch.max(logits, 1)
+        total_correct += (predicted == labels[:, batch]).sum().item()
+    acc = 1.0 * total_correct / total
+    return acc
+
+
+def main():
+    """Load dataset and train the model."""
+    # Load cmd line args
+    args = parse_args()
+    print(args)
+    # Load config file
+    model_cfg = load_config_file(args.model_cfg)
+    train_cfg = load_config_file(args.train_cfg)
+    set_seed(train_cfg["seed"])
+
+    # load and preprocess dataset
+    if args.gpu < 0:
+        device = "cpu"
+        cuda = False
+    else:
+        device = args.gpu
+        cuda = True
+
+    print("Training with a fixed epsilon = 0")
+
+    dataset = gli.dataloading.get_gli_dataset(args.dataset, args.task)
+    train_dataset = dataset[0]
+    val_dataset = dataset[1]
+    # test_dataset = dataset[2]
+
+    # create dataloader
+    train_loader = GraphDataLoader(train_dataset,
+                                   batch_size=128,
+                                   pin_memory=torch.cuda.is_available())
+    val_loader = GraphDataLoader(val_dataset,
+                                 batch_size=128,
+                                 pin_memory=torch.cuda.is_available())
+
+    # create GIN model
+    in_size = train_dataset[0][0].ndata["NodeFeature"].shape[1]
+    out_size = train_dataset.num_labels
+    print("insize: ", in_size)
+    print("out_size: ", out_size)
+    model = generate_model(args, in_size, out_size, **model_cfg)
+    if cuda:
+        model.cuda()
+
+    # model training/validating
+    print("Training...")
+
+    # loss function, optimizer and scheduler
+    loss_fcn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
+
+    # training loop
+    for epoch in range(train_cfg["max_epoch"]):
+        model.train()
+        total_loss = 0
+        batch = 0
+        for batch, (batched_graph, labels) in enumerate(train_loader):
+            batched_graph = batched_graph.to(device)
+            labels = labels.to(device)
+            feat = batched_graph.ndata["NodeFeature"].float()
+            logits = model(batched_graph, feat)
+            loss = loss_fcn(logits, labels[:, batch])
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        scheduler.step()
+        train_acc = evaluate(train_loader, device, model)
+        valid_acc = evaluate(val_loader, device, model)
+        print(f"Epoch {epoch:05d} | Loss {total_loss / (batch + 1):.4f} | \
+                Train Acc. {train_acc:.4f} | Validation Acc. {valid_acc:.4f}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ benchmarks/GraphClassification/utils.py
@@ -0,0 +1,78 @@
+"""
+Utility functions in gli.
+
+References:
+https://github.com/dmlc/dgl/tree/master/examples/pytorch/gin
+"""
+
+import torch
+import random
+import numpy as np
+import yaml
+from models.gin import GIN
+import argparse
+
+
+def generate_model(args, in_size, out_size, **model_cfg):
+    """Generate required model."""
+    # create models
+    if args.model == "GIN":
+        model = GIN(in_size, model_cfg["hidden_dim"], out_size)
+    try:
+        model
+    except UnboundLocalError as exc:
+        raise NameError(f"model {args.model} is not supported yet.") from exc
+    else:
+        return model
+
+
+def load_config_file(path):
+    """Load yaml files."""
+    with open(path, "r", encoding="utf-8") as stream:
+        try:
+            parsed_yaml = yaml.full_load(stream)
+            print(parsed_yaml)
+            return parsed_yaml
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def set_seed(seed):
+    """Set random seed."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+
+
+def parse_args():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser(description="train for graph\
+                                                  classification")
+    parser.add_argument("--model-cfg", type=str,
+                        default="configs/model_default.yaml",
+                        help="The model configuration file path.")
+    parser.add_argument("--train-cfg", type=str,
+                        default="configs/train_default.yaml",
+                        help="The training configuration file path.")
+    parser.add_argument("--model", type=str, default="GIN",
+                        help="model to be used. GCN, GAT, MoNet,\
+                              GraphSAGE, MLP, LINKX, MixHop for now")
+    parser.add_argument("--dataset", type=str, default="cora",
+                        help="dataset to be trained")
+    parser.add_argument("--task", type=str,
+                        default="GraphClassification",
+                        help="task name. NodeClassification,\
+                        GraphClassification, LinkPrediction,\
+                        TimeDependentLinkPrediction,\
+                        KGRelationPrediction, NodeRegression.\
+                        KGEntityPrediction, GraphRegression,\
+                        for now")
+    parser.add_argument("--task-id", type=int, default=1,
+                        help="task id, starting from 1")
+    parser.add_argument("--gpu", type=int, default=-1,
+                        help="which GPU to use. Set -1 to use CPU.")
+    parser.add_argument("--verbose", type=bool, default=False,
+                        help="whether to print verbosely")
+    return parser.parse_args()
