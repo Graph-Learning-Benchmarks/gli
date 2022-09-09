959440c7f05fabd67e31c07ce4cc15ad8550f39a
GreatSnoopyMe <umlty@umich.edu>
2022-09-09 00:05:30 -0400

    Fixing train.py to lim's code

--- benchmarks/NodeClassification/utils.py
+++ benchmarks/NodeClassification/utils.py
@@ -30,6 +30,8 @@ from models.mixhop import MixHop
 from models.linkx import LINKX
 
 Models_need_to_be_densed = ["GCN", "GraphSAGE", "GAT", "MixHop", "LINKX"]
+Datasets_need_to_be_undirected = ["pokec", "genius", "penn94", "twitch-gamers"]
+Models_using_adamW = ["LINKX", "MixHop"]
 
 
 def generate_model(args, g, in_feats, n_classes, **model_cfg):
