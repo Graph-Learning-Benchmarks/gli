270595b31a38a33bd395bc1bb7b3c18467d98d4b
StevenGolden1203 <stevengolden@163.com>
2022-09-12 00:32:12 -0400

    fix style error

--- benchmarks/GraphClassification/train.py
+++ benchmarks/GraphClassification/train.py
@@ -16,26 +16,6 @@ from utils import generate_model, load_config_file,\
 from dgl.dataloading import GraphDataLoader
 
 
-def eval(model, device, loader, evaluator):
-    model.eval()
-    y_true = []
-    y_pred = []
-
-    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
-        batch = batch.to(device)
-
-        if batch.x.shape[0] == 1:
-            pass
-        else:
-            with torch.no_grad():
-                pred = model(batch)
-
-            y_true.append(batch.y.view(pred.shape).detach().cpu())
-            y_pred.append(pred.detach().cpu())
-
-    y_true = torch.cat(y_true, dim = 0).numpy()
-    y_pred = torch.cat(y_pred, dim = 0).numpy()
-
 def evaluate(dataloader, device, model, eval_func):
     """Evaluate model."""
     model.eval()
@@ -50,8 +30,8 @@ def evaluate(dataloader, device, model, eval_func):
         y_true.append(labels.detach().cpu())
         y_pred.append(logits.detach().cpu())
 
-    y_true = torch.cat(y_true, dim = 0).numpy()
-    y_pred = torch.cat(y_pred, dim = 0).numpy()
+    y_true = torch.cat(y_true, dim=0).numpy()
+    y_pred = torch.cat(y_pred, dim=0).numpy()
     return eval_func(y_pred, y_true)
 
 
