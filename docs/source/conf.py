9798068cb0775ac4416c810a26ee5303e65ff882
Xingjian Zhang <jimmyzxj@umich.edu>
2022-11-04 10:19:01 -0400

    Try insert

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
 
-sys.path.append(os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
