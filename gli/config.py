0b5f6d68f793ddfdd39301499fa5b62fa7da6e70
Jiaqi Ma <jiaqima.mle@gmail.com>
2023-10-21 18:09:08 -0500

    update global_urls.json url

--- gli/config.py
+++ gli/config.py
@@ -4,5 +4,5 @@ from os.path import realpath, dirname, expanduser, join
 ROOT_PATH = dirname(dirname(realpath(__file__)))
 WARNING_DENSE_SIZE = 1e9
 DATASET_PATH = join(expanduser("~"), ".gli/datasets")
-GLOBAL_FILE_URL = "https://www.jiaqima.com/gli/global_urls.json"
+GLOBAL_FILE_URL = "https://jiaqima.github.io/gli/global_urls.json"
 SERVER_IP = "http://34.211.28.138"
