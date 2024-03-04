7336d6be8e40c78d916d23d1eda9d179db1e0cd2
StevenGolden1203 <stevengolden@163.com>
2024-03-04 15:37:24 -0500

    remove raw_text_utils in gli.init

--- gli/__init__.py
+++ gli/__init__.py
@@ -5,6 +5,5 @@ from . import dataset
 from . import graph
 from . import task
 from . import utils
-from . import raw_text_utils
 from .dataloading import get_gli_graph, get_gli_task, \
     get_gli_dataset, combine_graph_and_task
